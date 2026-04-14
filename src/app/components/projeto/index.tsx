4a3a2fd6004d4a765cce68d463a2e0a4a7fdee71
Fernando <fernandoeqp59@gmail.com>
2026-04-14 17:04:04 -0300

    ok

--- src/app/components/projeto/index.tsx
+++ src/app/components/projeto/index.tsx
@@ -20,7 +20,7 @@ import ImgCoinsDev from "@/assets/ImgCoinsDev.png";
 import ImgGestaoColinas from "@/assets/imgGestaoColinas.png";
 import ImgFlume from "@/assets/imgFlume1.png";
 import ImgAxion from "@/assets/imgAxion.png";
-import ImgColinasAtivo from "@/assets/imgColinasAtivo.png";
+import ImgColinasAtivo from "@/assets/imgcolinasativo.png";
 import { FolderGit2 } from "lucide-react";
 import Title from "../title";
 import { AuroraText } from "@/components/ui/aurora-text";
